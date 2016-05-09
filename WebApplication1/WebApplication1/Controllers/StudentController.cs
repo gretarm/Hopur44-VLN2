0460aa6d010d190871f720c4e4439c9189d8d7e7
Ragnarstefanss <ragnars15@ru.is>
2016-05-09 11:13:53 +0000

    Students sees courses

--- WebApplication1/WebApplication1/Controllers/StudentController.cs
+++ WebApplication1/WebApplication1/Controllers/StudentController.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Net;
 using System.Web.Mvc;
+using WebApplication1.Models.Entities;
+using WebApplication1.DAL;
+using WebApplication1.Controllers;
+using WebApplication1.Services;
 
 namespace WebApplication1.Controllers
 {
     public class StudentController : Controller
     {
+        private MooshakContext db = new MooshakContext();
         // GET: Student
         public ActionResult Index()
         {
-            return View();
+            IEnumerable<Course> cources = (from item in db.Courses
+                                           orderby item.Title ascending
+                                           select item).ToList();
+
+            return View(cources);
         }
     }
 }
