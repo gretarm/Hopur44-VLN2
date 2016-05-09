20628b2e2254337381b325c5674afe11c424915b
egillj88 <egillj88@gmail.com>
2016-05-09 14:43:37 +0000

    teachet sees courses and added partial view

--- WebApplication1/WebApplication1/Controllers/TeacherController.cs
+++ WebApplication1/WebApplication1/Controllers/TeacherController.cs
@@ -1,17 +1,25 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.DAL;
+using WebApplication1.Models.Entities;
 
 namespace WebApplication1.Controllers
 {
     public class TeacherController : Controller
     {
+
+		private MooshakContext db = new MooshakContext();
         // GET: Teacher
         public ActionResult Index()
         {
-            return View();
+			IEnumerable<Course> courseList = (from c in db.Courses
+											  orderby c.Title ascending
+											  select c).ToList();
+            return View(courseList);
         }
     }
 }
