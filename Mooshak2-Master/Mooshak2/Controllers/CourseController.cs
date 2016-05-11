c19ede18fd03a5d824ba22bd5710dc022f4a1cf1
Ragnarstefanss <ragnars15@ru.is>
2016-05-11 00:46:06 +0000

    Changed Directory /Student/Course/ID to /Course/ID/

---
+++ Mooshak2-Master/Mooshak2/Controllers/CourseController.cs
@@ -0,0 +1,46 @@
+﻿using System.Data;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Mooshak2.Models.Entities;
+
+namespace Mooshak2.Controllers
+{
+    public class CourseController : Controller
+    {
+        public ActionResult ID(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Course cor = (from item in DatabaseConnection.Db.Courses
+                          where item.CourseID == id.Value
+                          select item).SingleOrDefault();
+
+            if (cor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cor);
+        }
+        public ActionResult Assignments(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Assignment asi = (from assignment_item in DatabaseConnection.Db.Assignments
+                          where assignment_item.CourseID == id.Value
+                          select assignment_item).SingleOrDefault();
+
+            if (asi == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asi);
+        }
+    }
+}
--- Mooshak2-Master/Mooshak2/Controllers/StudentController.cs
+++ Mooshak2-Master/Mooshak2/Controllers/StudentController.cs
@@ -18,39 +18,19 @@ namespace Mooshak2.Controllers
     [Authorize(Roles = "Student")]
     public class StudentController : Controller
     {
-        
+        CoursesService course_service = new CoursesService();
+
         // GET: Student
         public ActionResult Index()
         {
-            /*
-            string userId = User.Identity.GetUserId();
-            IEnumerable<Enrollment> cources = (from item in db.Enrollments
-                                           where item.UserID = userId
-                                               select item).ToList();
-           */
+            //var userId = User.Identity.GetUserId();
+            //var user = course_service.GetCoursesForUserId(userId);
+
+
             IEnumerable<Course> courses = (from c in DatabaseConnection.Db.Courses
                                            orderby c.Title ascending
                                            select c).ToList();
-
             return View(courses);
-        }
-        [Authorize]
-        public ActionResult Courses(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
-            Course cor = (from item in DatabaseConnection.Db.Courses
-                          where item.CourseID == id.Value
-                          select item).SingleOrDefault();
-
-            if (cor == null)
-            {
-                return HttpNotFound();
-            }
-            return View(cor);
         }
     }
 }
