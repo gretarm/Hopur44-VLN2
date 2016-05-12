c55849b539dceffb095b9b5c9f4a616425ebee58
Ragnar Stefánsson <ragnars15@ru.is>
2016-05-12 14:50:36 +0000

    Find student and teachers in specific course

---
+++ Mooshak2-Master/Mooshak2/Models/ViewModels/StudentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Mooshak2.Models.Entities;
+
+namespace Mooshak2.Models.ViewModels
+{
+    public class StudentViewModel
+    {
+        public Enrollment ID { get; set; }
+        public Course Title { get; set; }
+        public ApplicationUser Name { get; set; }
+    }
+}
---
+++ Mooshak2-Master/Mooshak2/Models/ViewModels/TeacherViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Mooshak2.Models.Entities;
+
+namespace Mooshak2.Models.ViewModels
+{
+    public class TeachersViewModel
+    {
+        public Teachment ID { get; set; }
+        public Course Title { get; set; }
+        public ApplicationUser Name { get; set; }
+    }
+}
